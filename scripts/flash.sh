f9386e28f2bf76adc9ec10da993346a2bc90d085
Anthony Ngo <atonyngox+github@gmail.com>
2026-05-28 00:41:19 +0100

    Minor script update

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -13,12 +13,15 @@ mv "corne_left nice_view_adapter nice_view-nice_nano_v2-zmk.uf2" /media/corne
 umount /media/corne
 echo "Finished flashing left side"
 
-echo "Sleeping for 20 seconds..."
+echo "Sleeping for 10 seconds..."
 echo "Plug-in the right side"
-sleep 20
+sleep 10
 
 echo "Flashing right side..."
 mount /dev/sda /media/corne
 mv "corne_right nice_view_adapter nice_view-nice_nano_v2-zmk.uf2" /media/corne
 umount /media/corne
 echo "Finished flashing right side"
+
+rm firmware.zip
+echo "Deleted firmware.zip"
